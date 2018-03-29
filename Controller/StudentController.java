3ccc30da119a4c961f6caa57f7ebfd23cc1f410f
Neil <choi.neil@gmail.com>
2018-03-28 23:52:09 -0700

    First Run

---
+++ Controller/StudentController.java
@@ -0,0 +1,27 @@
+package com.Neil.Controller;
+
+import com.Neil.Entity.Student;
+import com.Neil.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+/**
+ * Created by Neil on 3/25/2018.
+ */
+
+@RestController
+@RequestMapping("/Students")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Collection<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+}
